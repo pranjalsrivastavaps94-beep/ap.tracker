0cb98ea2750376158ce43b036c83783c4388c213
pranjalsrivastavaps94-beep <pranjalsrivastava.ps94@gmail.com>
2026-03-07 02:09:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -24,14 +24,18 @@ import {
 /* =========================
    FIREBASE CONFIG PASTE HERE
    ========================= */
-const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
-};
+
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyA6mhqKVbfiC5aKooDqPCC2wQqBqJwhdDE",
+    authDomain: "pranjal-study-tracker.firebaseapp.com",
+    projectId: "pranjal-study-tracker",
+    storageBucket: "pranjal-study-tracker.firebasestorage.app",
+    messagingSenderId: "708890835472",
+    appId: "1:708890835472:web:c64dbb80339adadfcc6895"
+  };
+
 
 const FIREBASE_READY = firebaseConfig.apiKey && firebaseConfig.apiKey !== "YOUR_API_KEY";
 const LOCAL_STORAGE_KEY = "study_tracker_pro_local_v3";
@@ -1456,3 +1460,4 @@ function escapeHtml(value) {
     .replaceAll('"', "&quot;")
     .replaceAll("'", "&#039;");
 }
+
